977ada28807f6c02385b718b3c5f37df7dad2a49
Natalie <ziweih1993@gmail.com>
2024-08-21 16:53:01 -0400

    Create all pages and router the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,63 @@
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import * as Pages from './pages';
 
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Pages.HomeLayout />
+  },
+  {
+    path:'/addEvent',
+    element:<Pages.AddEvent />
+  },
+  {
+    path: '/admin',
+    element: <Pages.Admin />
+  },
+  {
+    path: '/allEvents',
+    element: <Pages.AllEvents />
+  },
+  {
+    path: '/dashboardLayout',
+    element: <Pages.DashboardLayout />
+  },
+  {
+    path: '/delete',
+    element: <Pages.DeleteEvent />
+  },
+  {
+    path: '/error',
+    element: <Pages.Error />
+  },
+  {
+    path: '/edit',
+    element: <Pages.EditEvent />
+  },
+  {
+    path: '/landing',
+    element: <Pages.Landing />
+  },
+  {
+    path:'/login',
+    element:<Pages.Login />
+  },
+  {
+    path: '/profile',
+    element: <Pages.Profile />
+  },
+  {
+    path:'/register',
+    element:<Pages.Register />
+  },
+  {
+    path:'/stats',
+    element:<Pages.Stats />
+  },
+])
 const App = () => {
-  return (
-    <h1>Jobify App</h1>
-  );
+  return <RouterProvider router={router} />;
+  
 }
 
 export default App;
