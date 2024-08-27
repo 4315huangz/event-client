a41ccf93176705d32a8b49f1d18adf111ef4c947
Natalie <ziweih1993@gmail.com>
2024-08-27 14:38:31 -0400

    Work on Landing page

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,8 +1,8 @@
-import {Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const HomeLayout = () => {
     return <div>
-        <nav>navbar</nav>
+        <nav></nav>
         <Outlet />
     </div>;
 };
